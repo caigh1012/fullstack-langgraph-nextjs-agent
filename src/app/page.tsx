0346626dff6356c26bd62eb0971d92912f19f534
caigh1012 <19925338175@163.com>
2026-07-18 11:19:20 +0800

    feat: add shadcn/ui

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,11 @@
+'use client';
+import { Button } from '@/components/ui/button';
+
 export default function Home() {
-  return <div>HELLO WORLD</div>;
+  return (
+    <div>
+      HELLO WORLD
+      <Button>Click me</Button>
+    </div>
+  );
 }
